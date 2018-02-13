0a2c706e2e1711faf092b5ed9acec27d5f57ff8c
homebrewdev <xstall@yandex.ru>
2018-02-13 21:19:46 +0400

    Update ActivityAbout.java

--- app/src/main/java/com/example/toolbarex/ActivityAbout.java
+++ app/src/main/java/com/example/toolbarex/ActivityAbout.java
@@ -3,9 +3,6 @@ package com.example.toolbarex;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Администратор on 04.04.2016.
- */
 public class ActivityAbout extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
